38012ec3f63498d22cd5a1f25b2a891ea1f12fa4
ryuken73 <ryuken73@gmail.com>
2022-03-22 00:23:14 +0900

    fix: re-calculate inner height of viewport not triggered after tablet rotated

--- src/hooks/useMediaQueryEasy.js
+++ src/hooks/useMediaQueryEasy.js
@@ -1,9 +1,13 @@
 import useMediaQuery from '@mui/material/useMediaQuery';
 
 function useMediaQueryEasy() {
-  const isLandscape  = useMediaQuery(`(min-width:600px)`);
+//   const isLandscape  = useMediaQuery(`(min-width:600px)`);
+  const isLandscape = useMediaQuery(`(orientation: landscape)`);
+  const isPortrait = useMediaQuery(`(orientation: portrait)`);
   return {
-    isLandscape
+    isLandscape,
+    isPortrait
+    // isLandscapeTablet
   }
 }
 
